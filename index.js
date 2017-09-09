01532015044b9813ec5d69d1db3d5dbb0b7b97e9
cth103 <cthdev@email.com>
2017-09-09 13:21:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,56 +2,8 @@
 const discord = require("discordie"); //i use discordie b'cuz lazy
 var user = new discord(); //user object
 var start = ["Mj", "its_a_bot"];
-var length = [59, 64, 57]; //some tokens are 59 chars longs and some are 64 chars long, mainly 59 though.
-function lol(token, name) { 
-    try{
-    user.connect({
-        token: token
-    });   
-    client.Dispatcher.on("GATEWAY_READY", a => {
-        console.log("Connected as " + client.User.username + " with token: " + token);
-        if (client.User.username = name) {
-            console.log("Yay! Found " + client.User.username + "'s token, it's "+ token);
-        } else {
-            console.log(" User " + client.User.username + " with token: "+ token + " is not target"); //rape joke
-        }
-    });
-    } catch (err) {
-    gener8(name);
-    }
-}
-function gener8(name) { 
-    try{
-         //you can joke about murder but not about rape :'(
-         var token = ""; //tokin
-         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ.abcdefghijklmnopqrstuvwxyz-0123456789_"; //lol
-         //stealing from stackoverflow since 2007
-         //change to length[2] for 64 chars
-         //if user use this
-         /* for (var i = 0; i < length[3]; i++) 
-              token += possible.charAt(Math.floor(Math.random() * possible.length));
-         */
-         for (var i = 0; i < length[1]; i++) 
-              token += possible.charAt(Math.floor(Math.random() * possible.length));
-        //if user:
-        //do(start[1] + token, name)
-        lol(token, name);
-    } catch (err) {
-        console.log("lol error");
-    }
-}
-function do_it(name) { 
-    try{
-         gener8(name);
-    } catch (err) {
-        console.log("lol error");
-    }
-}//yeah ok
-const discord = require("discordie"); //i use discordie b'cuz lazy
-var user = new discord(); //user object
-var start = ["Mj", "its_a_bot"];
 var length = [59, 64, 57, 80]; //some tokens are 59 chars longs and some are 64 chars long, mainly 59 though.
-function try_to_do_a_thing(token, name) { 
+function try_to_do_a_thing(token) { 
     try{
     console.log("trying "+token)
     user.connect({
@@ -59,35 +11,37 @@ function try_to_do_a_thing(token, name) {
     });   
     console.log("ok");
     client.Dispatcher.on("GATEWAY_READY", a => {
-        console.log("Connected as " + client.User.username + " with token: " + token);
-        if (client.User.username = name) {
-            console.log("Yay! Found " + client.User.username + "'s token, it's "+ token);
-        } else {
-            console.log(" User " + client.User.username + " with token: "+ token + " is not target"); //rape joke
-        }
+        console.log("Yay! Found " + client.User.username + "'s token, it's "+ token);
     });
     } catch (err) {
-    console.log("retrying");
-    gener8(name);
+    console.log("Can't Login /shrug");
+    gener8();
     }
 }
-function gener8(name) { 
+function gener8() { 
     try{
         console.log("generating token");
         //you can joke about murder but not about rape :'(
         //token alphabet: ABCDEFGHIJKLMNOPQRSTUVWXYZ.abcdefghijklmnopqrstuvwxyz-0123456789_
         console.log("trying token");
-        //try_to_do_a_thing(make_token2(), name);
-        try_to_do_a_thing(make_token(), name);
+        var chance = Math.random() * 100;
+        if (chance < 80) {
+            try_to_do_a_thing(make_token2());
+        } else if (chance < 85) {
+            try_to_do_a_thing(make_token());
+        } else {
+            try_to_do_a_thing(make_token2());
+        }
+        
     } catch (err) {
         console.log("lol error");
     }
 }
-function do_it(name) { 
+function do_it() { 
     console.log("hi!");
     try{
         console.log("generating!");
-        gener8(name);
+        gener8();
     } catch (err) {
         console.log("lol error");
     }
@@ -111,7 +65,5 @@ function make_token2() {
 
   return "Mj"+text;
 }
-do_it("b1nzy"); //b1nzy dont ratelimit me ;-;
-//dabbit is the god
-do_it("b1nzy"); //b1nzy dont ratelimit me ;-;
+do_it(); //b1nzy dont ratelimit me ;-;
 //dabbit is the god
