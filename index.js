1983cd324def7d891c703994a38c0ef259723fa5
cth103 <cthdev@email.com>
2017-09-09 13:49:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const discord = require("discordie"); //i use discordie b'cuz lazy
 var user = new discord(); //user object
 var start = ["Mj", "its_a_bot"];
-var length = [59, 64, 57, 80]; //some tokens are 59 chars longs and some are 64 chars long, mainly 59 though.
+var length = [59, 64, 58, 80]; //some tokens are 59 chars longs and some are 64 chars long, mainly 59 though.
 function try_to_do_a_thing(token) { 
     try{
     console.log("trying "+token)
@@ -12,17 +12,21 @@ function try_to_do_a_thing(token) {
     console.log("ok");
     client.Dispatcher.on("GATEWAY_READY", a => {
         console.log("Yay! Found " + client.User.username + "'s token, it's "+ token);
+        process.exit();
     });
     } catch (err) {
-    console.log("Can't Login /shrug");
+    console.log("Can't Login");
+    require('system-sleep')(60);
     gener8();
     }
 }
+
 function gener8() { 
     try{
         console.log("generating token");
         //you can joke about murder but not about rape :'(
-        //token alphabet: ABCDEFGHIJKLMNOPQRSTUVWXYZ.abcdefghijklmnopqrstuvwxyz-0123456789_
+        //a token: MzU1NDM0ODE2ODQ3NDc4ODA0.DJVlgw.iHbkSIBIPRA7uPL28Jc_so_EiH0
+        //token alphabet: MABCZDEFGHIJK.LMNOPQRSTUVWXYZabcd.efghijklmnopqrstuvwxyz-0123456789_-gabctAbhxdBJtDISCORDCORPORATIONPLEASENOSTEAL
         console.log("trying token");
         var chance = Math.random() * 100;
         if (chance < 80) {
@@ -30,11 +34,11 @@ function gener8() {
         } else if (chance < 85) {
             try_to_do_a_thing(make_token());
         } else {
-            try_to_do_a_thing(make_token2());
+            try_to_do_a_thing(make_token());
         }
         
     } catch (err) {
-        console.log("lol error");
+        console.log("lol error"+err);
     }
 }
 function do_it() { 
@@ -48,7 +52,7 @@ function do_it() {
 }
 function make_token() {
   var text = "";
-  var possible = "MJABCDEFGHIJKLMNOPQRSTUVWXYZ.abcdefghijklmnopqrstuvwxyz-0123456789_";
+  var possible = "MABCZDEFGHIJK.LMNOPQRSTUVWXYZabcd.efghijklmnopqrstuvwxyz-0123456789_-gabctAbhxdBJtDISCORDCORPORATIONPLEASENOSTEAL";
 
   for (var i = 0; i < length[1]; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -58,12 +62,12 @@ function make_token() {
 
 function make_token2() {
   var text = "";
-  var possible = "MJABCDEFGHIJKLMNOPQRSTUVWXYZ.abcdefghijklmnopqrstuvwxyz-0123456789_";
+  var possible = "MABCZDEFGHIJK.LMNOPQRSTUVWXYZabcd.efghijklmnopqrstuvwxyz-0123456789_-gabctAbhxdBJtDISCORDCORPORATIONPLEASENOSTEAL";
 
   for (var i = 0; i < length[3]; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return "Mj"+text;
+  return "M"+text;
 }
 do_it(); //b1nzy dont ratelimit me ;-;
 //dabbit is the god
